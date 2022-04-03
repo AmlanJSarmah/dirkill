99782eeaeae82869f167624f5cadf09b0aab6de2
A-J-Sarmah <devajsarmah@gmail.com>
2022-04-03 14:16:43 +0530

    Created a Listbox to that will display all the folders🎉

--- src/dirkill.py
+++ src/dirkill.py
@@ -6,7 +6,9 @@ sg.theme('LightGrey1')
 layout = [
 	[sg.Text('Enter path',pad=(10,20)),sg.Input(expand_x=True)],
 	[sg.Text('Enter folder name',pad=(10,20)),sg.Input(expand_x=True)],
-	[sg.Button('Find folders',size=(15,2),pad=(215,20))]
+	[sg.Button('Find folders',size=(15,2),pad=(215,20),key="-STORE-")],
+	[sg.Listbox([],size=(30,30),expand_x=True,select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE,key="-SELECT-")],
+	[sg.Button('Delete',size=(15,2),pad=(215,20),key="-DELETE-")],
 ]
 
 window = sg.Window('dirkill',layout)
