86ba6c0275057126b2c532d257fcabf6683e5c5d
A-J-Sarmah <devajsarmah@gmail.com>
2022-04-03 16:47:12 +0530

    Added file traversing support

--- src/dirkill.py
+++ src/dirkill.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import os
+from utils import traverse_file_system
 
 # Path and folder name
 path = ''
@@ -13,7 +14,7 @@ layout = [
 	[sg.Text('Enter path',pad=(10,20)),sg.Input(expand_x=True,key="-PATH-")],
 	[sg.Text('Enter folder name',pad=(10,20)),sg.Input(expand_x=True,key="-FOLDER-")],
 	[sg.Button('Find folders',size=(15,2),pad=(215,20),key="-STORE-")],
-	[sg.Listbox(['A'],size=(30,30),expand_x=True,select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE,key="-SELECT-")],
+	[sg.Listbox([],size=(30,30),expand_x=True,select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE,key="-SELECT-")],
 	[sg.Button('Delete',size=(15,2),pad=(215,20),key="-DELETE-")],
 ]
 
@@ -32,9 +33,13 @@ while True:
 		if values['-PATH-'] and values['-FOLDER-'] != '':
 			path = values['-PATH-']
 			folder = values['-FOLDER-']
-			print(path,folder)
+			select_list = traverse_file_system(path,folder)
+			window['-SELECT-'].Update(values=select_list)
+
+
 	if event == '-DELETE-':
 		if values['-SELECT-'] != []:
 			print("Hi for now")
+
 
 window.close()
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
 import os
+import platform
+
+def traverse_file_system(path,folder_name):
+	platform_sys = platform.system()
+	directories = []
+	try:
+		for dirpaths, dirnames, filenames in os.walk(rf'{path}'):
+			if dirnames.count(folder_name) > 0:
+				if platform_sys=='Windows': 
+					directories.append(dirpaths +  '\\' + folder_name)
+				else:
+					directories.append(dirpaths + '/' + folder_name)
+		return directories
+	except:
+		return []
